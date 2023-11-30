651d5ec50d96e38512e99f31cf3e431bacd53b04
Howard <howard@brightpage.tech>
2023-11-30 20:49:07 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>props.course_name</h1>
+    </>
+  )
+}
+
+
+const Content = (props) => {
+  return (
+    <>
+      <p>{props.part} contains {props.exercises} exercises.</p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <>
+      <p>The total number of exercises is {props.total}</p>
+    </>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course_name={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total total={exercises1+exercises2+exercises3} />
+    </div>
+  )
+}
+
+export default App
